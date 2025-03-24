53f7cea2db80cdc059eaa21f5fa2f2a32d978e16
OM TRIPATHI <tripathi111om@gmail.com>
2025-03-25 02:06:12 +0530

    testing deploy

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build frontend
-cd client
-npm run build
-
-# Copy files to EC2
-scp -i your-key.pem -r dist/ ubuntu@3.93.219.99:~/codeprepai/client/
-scp -i your-key.pem -r server/* ubuntu@3.93.219.99:~/codeprepai/server/ 
